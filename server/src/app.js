61e6a09792b2133ec5b35b83cbc2f7ce365102b6
nat <nattavutow@gmail.com>
2019-03-11 16:57:01 +0700

    up to web git

--- server/src/app.js
+++ server/src/app.js
@@ -1,43 +1,20 @@
-let express = require('express')
-let bodyParser=require('body-parser')
-
-const app = express()
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-
-
-app.get('/status',function(req,res){
-    res.send('Hello nodejs server')
-})
-
-app.get('/hello/:person',function(req,res){
-    console.log('Hello - '+req.params.person)
-    res.send('Sey Hello with '+ req.params.person)
-})
-
-app.get('/user/:userId',function(req,res){
-    res.send('ดูข้อมูลผู้ใช้งาน')
-})
-
-app.get('/users',function(req,res){
-    res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
-})
-
-app.post('/user/',function(req,res){
-    res.send('ทำการสร้างผู้ใช้งาน: '+ JSON.stringify(req.body))
-})
-
-app.put('/user/:userId',function(req,res){
-    res.send('ทำการแก้ไขผู้ใช้งาน: '+req.params.userId +':'+ JSON.stringify(req.body))
-})
-
-app.delete('/user/:userId',function(req,res){
-    res.send('ทำการลบผู้ใช้งาน : '+ req.params.userId+':'+ JSON.stringify(req.body))
-})
-
-let port = 8080
-
-app.listen(port,function(){
-    console.log('server runing on '+ port)
-})
+let express = require('express') 
+let bodyParser = require('body-parser') 
+ 
+const app = express() 
+ 
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({extended: true})) 
+ 
+require('./routes')(app) 
+ 
+app.get('/status', function (req, res ){   res.send('Hello nodejs server') }) 
+ 
+app.get('/hello/:person', function (req,res) {   console.log('hello - ' + req.params.person) 
+res.send('sey hello with ' + req.params.person) }) 
+ 
+app.post('/hello', function (req, res) {   res.send('OK  you post - ' + req.body.name) }) 
+ 
+let port = 8081 
+ 
+app.listen(port, function () {   console.log('server running on ' + port) })
